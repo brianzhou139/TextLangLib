3e5dc4f3afb815b5183c83ec4580933b56ba3192
Brian Zhou <39838697+brianzhou139@users.noreply.github.com>
2021-05-08 08:03:06 +0100

    Add files via upload
    sample input dt

---
+++ build/CMakeFiles/tgcat-tester.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tgcat-tester.dir/tester.c.o"
+  "CMakeFiles/tgcat-tester.dir/json.c.o"
+  "CMakeFiles/tgcat-tester.dir/json-builder.c.o"
+  "tgcat-tester.pdb"
+  "tgcat-tester"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tgcat-tester.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
